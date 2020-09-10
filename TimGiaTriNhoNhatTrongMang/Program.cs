43feaf5661cdc184a383821b0a03db6806b23516
trinhnam12345z <trinhnam123456789z@gmail.com>
2020-09-10 21:01:27 +0700

    Tì giá trị nhỏ nhất trong mảng

---
+++ TimGiaTriNhoNhatTrongMang/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TimGiaTriNhoNhatTrongMang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = { 4, 12, 7, 8, 1, 0, 9 };
+            int index = MinValue(arr);
+            Console.WriteLine("The smallest element in the array is: " + arr[index]);
+
+        }
+        public static int MinValue(int[] array)
+        {
+
+            int min = array[0];
+            int index = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                    index = i;
+                }
+            }
+
+            return index;
+        }
+    }
+}
